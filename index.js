b347798f8a5e1369aed5b626c1c87f7a153cdc1e
ivansabila <ivanscond@gmail.com>
2024-08-25 22:49:42 +0800

    Make post API request handler

--- index.js
+++ index.js
@@ -27,6 +27,25 @@ app.get("/", async (req, res) => {
 app.post("/", async (req, res) => {
     console.log(req.body);
 
+    axios
+        .get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`)
+        .then(function (response) {
+            console.log(`total data : ${response.data.length}`);
+
+            var index = Math.floor(Math.random() * response.data.length);
+            const result = response.data[index];            
+
+            res.render("index.ejs", {
+                data: result,
+            });
+        })
+        .catch(function (error) {
+            console.error("Failed to make request:", error.message);
+            res.render("index.ejs", {
+                error: "No activities that match your criteria.",
+            });
+        });
+
     // Step 2: Play around with the drop downs and see what gets logged.
     // Use axios to make an API request to the /filter endpoint. Making
     // sure you're passing both the type and participants queries.
